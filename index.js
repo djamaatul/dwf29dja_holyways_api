0ea636da340ff4f06f13e237697f7618c83f7d40
D. Jamaatul Anbiya <djamaatul.anbiya@gmail.com>
2021-12-25 13:28:26 +0700

    add Register API

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const router = require('./src/routes/index');
 
 app.use(express.json());
 app.use('/api/v1/', router);
+app.use((req, res) => {
+	res.status(404).send({
+		status: 'failed',
+		message: '404 : Request not found',
+	});
+});
 
 app.listen(port, () => {
 	console.log('listen to port ', port);
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,7 @@
 const { users } = require('../../models');
 const joi = require('joi');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 exports.login = async (req, res) => {
 	const input = req.body;
@@ -15,7 +16,7 @@ exports.login = async (req, res) => {
 	if (error) {
 		return res.status(400).send({
 			status: 'failed',
-			message: error,
+			message: error.details[0].message,
 		});
 	}
 
@@ -52,6 +53,67 @@ exports.login = async (req, res) => {
 				user: {
 					fullName: userExist.fullName,
 					email: userExist.email,
+					token,
+				},
+			},
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({
+			status: 'failed',
+			message: 'Server Error',
+		});
+	}
+};
+
+exports.register = async (req, res) => {
+	const input = req.body;
+
+	const schema = joi.object({
+		email: joi.string().email().required(),
+		password: joi.string().min(6).required(),
+		fullName: joi.string().min(3).required(),
+	});
+
+	const { error, value } = schema.validate(input);
+
+	if (error) {
+		return res.status(400).send({
+			status: 'failed',
+			message: error.details[0].message,
+		});
+	}
+
+	try {
+		const userExist = await users.findAll({
+			where: {
+				email: input.email,
+			},
+		});
+		if (userExist.length > 0) {
+			return res.status(409).send({
+				status: 'failed',
+				message: 'user email already exist',
+			});
+		}
+
+		const salt = await bcrypt.genSalt(8);
+
+		const hashedPassword = await bcrypt.hash(input.password, salt);
+
+		const newUser = await users.create({
+			email: input.email,
+			password: hashedPassword,
+			fullName: input.fullName,
+		});
+
+		const token = jwt.sign({ id: newUser.dataValues.id }, process.env.SECRET_KEY);
+
+		res.status(200).send({
+			status: 'success...',
+			data: {
+				user: {
+					fullName: input.fullName,
 					token,
 				},
 			},
